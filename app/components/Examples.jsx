330bedb8e680dc19f93e31ede581d61d0b4e9032
Pablo Pareja <pabloparejatobes@gmail.com>
2017-01-05 19:12:22 +0100

    Style about page & link to resources

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Heare are a few locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Granada'>Granada, ES</Link>
+        </li>
+        <li>
+          <Link to='/?location=Malaga'>Malaga, ES</Link>
+        </li>
+      </ol>
+    </div>
+
   );
 };
 
